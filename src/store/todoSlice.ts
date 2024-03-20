b861f0faaff7e6a5e17da9525225640dca00d06b
Geidtiphong Singseewo <geidtiphong.singseewo@krungsrinimble.com>
2024-03-20 14:18:39 +0700

    feat: create todoSlice

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,46 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit';
+import TodoStore from './TodoStore';
+import {TodoItemType} from '../types/genericTypes';
+
+const intialState = TodoStore;
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState: intialState,
+  reducers: {
+    createTodo: (state, action: {payload: {title: string}; type: string}) => {
+      state.todos = state.todos.concat([
+        {title: action.payload.title, id: nanoid()},
+      ]);
+    },
+
+    updateTodo: (state, action: {payload: TodoItemType; type: string}) => {
+      state.todos = state.todos.map((todoItem: TodoItemType) => {
+        if (todoItem.id === action.payload.id) {
+          todoItem.title = action.payload.title;
+
+          return todoItem;
+        }
+        return todoItem;
+      });
+
+      state.todos.concat([action.payload]);
+    },
+
+    deleteTodo: (state, action: {payload: {id: string}; type: string}) => {
+      state.todos = state.todos.filter(
+        (todoItem: TodoItemType) => todoItem.id !== action.payload.id,
+      );
+    },
+  },
+});
+
+const {createTodo, updateTodo, deleteTodo} = todoSlice.actions;
+
+export const TodoAction = {
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
+
+export default todoSlice.reducer;
