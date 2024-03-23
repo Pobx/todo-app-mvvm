60bb1f7c0452109e0d55495ba9287d7a9d6acf2b
Geidtiphong Singseewo <geidtiphong.singseewo@krungsrinimble.com>
2024-03-23 10:16:21 +0700

    fix: hard code value for property name

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,32 +1,32 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {screenMap} from './screenMap';
 import {HomeTodoScreen, NewTodoScreen, UpdateTodoScreen} from '../screens';
+import {RootStackParamList} from './rootStackParamList';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppContainer = () => {
-  const Stack = createNativeStackNavigator();
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name={screenMap.HomeTodo}
+          name="HomeTodo"
           component={HomeTodoScreen}
           options={{headerShown: false}}
         />
+
+        <Stack.Screen
+          name="NewTodo"
+          component={NewTodoScreen}
+          options={{headerShown: false}}
+        />
+
+        <Stack.Screen
+          name="UpdateTodo"
+          component={UpdateTodoScreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
-
-      <Stack.Screen
-        name={screenMap.NewTodo}
-        component={NewTodoScreen}
-        options={{headerShown: false}}
-      />
-
-      <Stack.Screen
-        name={screenMap.UpdateTodo}
-        component={UpdateTodoScreen}
-        options={{headerShown: false}}
-      />
     </NavigationContainer>
   );
 };
